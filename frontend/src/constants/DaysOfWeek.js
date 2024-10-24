0e19182989476cf98be9ef0601c5ec9307ff05ef
Jing Huang <jinghuang@Jings-MacBook-Air.local>
2024-10-24 16:44:02 -0400

    Add images to pages to improve ui.

---
+++ frontend/src/constants/DaysOfWeek.js
@@ -0,0 +1,31 @@
+export const DAYSOFWEEK = [
+  { label: "Monday", value: "Monday" },
+  { label: "Tuesday", value: "Tuesday" },
+  { label: "Wednesday", value: "Wednesday" },
+  { label: "Thursday", value: "Thursday" },
+  { label: "Friday", value: "Friday" },
+  { label: "Saturday", value: "Saturday" },
+  { label: "Sunday", value: "Sunday" },
+];
+
+export const exerciseTitles = [
+  "Bench Press",
+  "Deadlift",
+  "Squats",
+  "Pull-ups",
+  "Yoga",
+  "Running",
+  "Cycling",
+  "Plank",
+  "Hiking",
+  "Walking",
+];
+
+export const workoutTitles = [
+  "Strength Workout 1",
+  "Strength Workout 2",
+  "Cardio Workout",
+  "Flexibility and Core",
+  "Outdoor Activity",
+  "Active Recovery",
+];
