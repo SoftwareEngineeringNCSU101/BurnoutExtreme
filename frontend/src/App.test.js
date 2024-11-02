8a01c1b915bc04e2a51bddaf5070a3acb25de1e0
Jing Huang <jinghuang@Jings-MacBook-Air.local>
2024-11-01 22:08:34 -0400

    Resolved one failed file.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './components/App';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from './components/App'; 
+import { ThemeProvider } from './components/ThemeContext';
+import { GoogleOAuthProvider } from '@react-oauth/google'; 
+
+const clientId = 'YOUR_GOOGLE_CLIENT_ID';
 
 test('renders the landing page', () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <GoogleOAuthProvider clientId={clientId}> 
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </GoogleOAuthProvider>
+    </MemoryRouter>
+  );
+
 });
