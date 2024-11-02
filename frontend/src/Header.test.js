517ccf48a22de12ae1a7ecc68356a6fdd0e3ea24
Soundarya Khanapur <soundarya.m.khanapur@gmail.com>
2024-11-01 22:48:16 -0400

    UI test cases

--- frontend/src/Header.test.js
+++ frontend/src/Header.test.js
@@ -1,6 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import Header from './components/Header';
+// import { render, screen } from '@testing-library/react';
+// import Header from './components/Header';
+
+// test('renders the landing page', () => {
+//   render(<Header />);
+// });import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Header from './components/Header'; 
+import { ThemeProvider } from './components/ThemeContext';
+import { GoogleOAuthProvider } from '@react-oauth/google'; 
+
+const clientId = 'YOUR_GOOGLE_CLIENT_ID';
 
 test('renders the landing page', () => {
-  render(<Header />);
+  render(
+    <MemoryRouter>
+      <GoogleOAuthProvider clientId={clientId}> 
+        <ThemeProvider>
+          <Header />
+        </ThemeProvider>
+      </GoogleOAuthProvider>
+    </MemoryRouter>
+  );
+
 });
--- frontend/src/Router.test.js
+++ frontend/src/Router.test.js
@@ -1,6 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import Router from './components/Router';
+// import { render, screen } from '@testing-library/react';
+// import Router from './components/Router';
+
+// test('renders the landing page', () => {
+//   render(<Router />);
+// });
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Router from './components/Router'; 
+import { ThemeProvider } from './components/ThemeContext';
+import { GoogleOAuthProvider } from '@react-oauth/google'; 
+
+const clientId = 'YOUR_GOOGLE_CLIENT_ID';
 
 test('renders the landing page', () => {
-  render(<Router />);
+  render(
+    <MemoryRouter>
+      <GoogleOAuthProvider clientId={clientId}> 
+        <ThemeProvider>
+          <Router />
+        </ThemeProvider>
+      </GoogleOAuthProvider>
+    </MemoryRouter>
+  );
+
 });
